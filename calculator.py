8c4571da207b9854e89d55a43e3c1dbbb028c2c5
sclazenby <slazenby@hsph.harvard.edu>
2020-01-13 17:14:21 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Defining multiplication
+def multiply(a,b):
+	return a * b
+
+# Defining division
+def divide(a,b):
+	return a / b
+
+# Defining addition
+def add(a,b):
+	return a + b
+
+# Defining subtraction
+def substract(a,b):
+	return a - b
