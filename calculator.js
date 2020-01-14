5bfa38cdd70d019e2fd408bba5fcae35680adb1d
sclazenby <slazenby@hsph.harvard.edu>
2020-01-13 23:01:23 -0500

    added four functions to calculator in js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// Defining multiplication
+function multiply(a,b) {
+	return a * b
+}
+
+// Defining division
+function divide(a,b) {
+	return a / b
+}
+
+// Defining addition
+function add(a,b) {
+	return a + b
+}
+
+// Defining subtraction
+function substract(a,b) {
+	return a - b
+}
